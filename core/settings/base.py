0237d2902c576c6cdbfd25da9e23c477b927c3df
f0laf0lu <ofolafolu@gmail.com>
2025-05-05 20:04:53 +0100

    custom header middleware

--- core/settings/base.py
+++ core/settings/base.py
@@ -59,6 +59,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "apps.userauth.middleware.CustomHeaderMiddleware",
 ]
 
 ROOT_URLCONF = 'core.urls'
