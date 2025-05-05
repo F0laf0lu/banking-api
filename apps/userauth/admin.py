27e2cdbd0faa098131bcaf1fe44abd9925995a12
f0laf0lu <ofolafolu@gmail.com>
2025-05-05 19:20:51 +0100

    user creation, change form. User admin

--- apps/userauth/admin.py
+++ apps/userauth/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
+from .models import User
+from .forms import UserChangeForm, UserCreationForm
 
-# Register your models here.
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+    model = User
+    list_display = [
+        "email",
+        "username",
+        "first_name",
+        "last_name",
+        "is_staff",
+        "is_active",
+        "role",
+    ]
+    list_filter = ["email", "is_staff", "is_active", "role"]
+    search_fields = ["email", "username", "first_name", "last_name"]
+    ordering = ["email"]
